8b38ea88255c864d56bbfc16a0f00256b301e194
dev-axel <axel@n-vibe.fr>
2022-06-08 17:58:25 +0200

    First commit

---
+++ IndoorKit.framework/Headers/IndoorKit.h
@@ -0,0 +1,18 @@
+//
+//  IndoorKit.h
+//  IndoorKit
+//
+//  Created by MAC PRO N-VIBE on 03/06/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for IndoorKit.
+FOUNDATION_EXPORT double IndoorKitVersionNumber;
+
+//! Project version string for IndoorKit.
+FOUNDATION_EXPORT const unsigned char IndoorKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IndoorKit/PublicHeader.h>
+
+
